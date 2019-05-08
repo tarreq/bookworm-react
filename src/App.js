6d438b2deaca66a21c70f8c2a408b232d5701dd5
tarreeq <tarreeq@gmail.com>
2019-05-09 00:03:27 +0200

    Redux setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route } from 'react-router-dom'
 import HomePage from './components/pages/HomePage'
 import LoginPage from './components/pages/LoginPage'
+import SampleLogin from './components/pages/SampleLogin'
 
 const App = () => <div className="ui container">
     <Route path="/" exact component={HomePage} />
     <Route path="/login" exact component={LoginPage} />
+    <Route path="/slogin" exact component={SampleLogin} />
 </div>
 
 export default App;
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -5,7 +5,15 @@ function HomePage() {
   return (
     <div>
       <h1>Home Page!</h1>
+      <div>
       <Link to="/login">Login</Link>
+      </div>
+      
+      
+      <div>
+        <Link to="/slogin">Sample Login</Link>
+      </div>
+      
     </div>
   )
 }
